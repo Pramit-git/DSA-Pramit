72f6e5a52fbe370e03ef7fd85f62ee4264b4d841
Pramit git <pramitsarkarclg@gmail.com>
2025-07-26 23:51:16 +0530

    arr initialisation

---
+++ arrinitialization.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[4] = {0,1,2,3};//always we need to specify the number of elements present in array while ini.
+    for(int  i = 0;i<=3; i++){
+        cout<<arr[i]<<endl;//printing our arr
+    }
+}
